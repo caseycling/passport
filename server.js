23f92cd52de26c5c9019d03238bbd6a488e492ff
Casey <caseyclinga@gmail.com>
2021-08-04 14:13:55 -0700

    Included bcrypt for registration

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+
+const users = [];
 
 app.set('view-enginge', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -12,10 +15,23 @@ app.get('/login', (req, res) => {
   res.render(`login.ejs`);
 });
 
-app.post('/register', (req, res) => {});
-
 app.get('/register', (req, res) => {
   res.render(`register.ejs`, { name: 'Casey' });
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/');
+  } catch (error) {
+    res.redirect('/register');
+  }
 });
 
 app.post('/register', (req, res) => {});
